175c4a1923bf6695791207eefba24760582ab59a
Roger Sperberg <rsperberg@gmail.com>
2017-09-07 14:02:03 -0400

    Initial commit

---
+++ challenges/chal-139-computerCompoundInterest.js
@@ -0,0 +1,18 @@
+// preImmersive-buildingBlocksMastery-139-computeCompoundInterest
+
+/*
+Write a function called “computeCompoundInterest”.
+
+Given a principal, an interest rate, a compounding frequency, and a time (in years), “computeCompoundInterest” returns the amount of compound interest generated.
+
+var output = computeCompoundInterest(1500, .043, 4, 6);
+console.log(output); // --> 438.8368221341061
+Reference:
+https://en.wikipedia.org/wiki/Compound_interest#Calculation_of_compound_interest
+This shows the formula used to calculate the total compound interest generated.
+*/
+
+function computeCompoundInterest(P, r, n, t) {
+  // P — principal, r — interestRate, n — compoundingFrequency, t — timeInYears
+  return P * (( 1 + r / n )**(n*t)) - P;
+}
