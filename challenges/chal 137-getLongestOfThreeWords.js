86a293ea5e4f0a529301faa8f7b2b635fe3812e5
Roger Sperberg <rsperberg@gmail.com>
2017-09-11 14:53:33 -0400

    Initial commit

---
+++ challenges/chal 137-getLongestOfThreeWords.js
@@ -0,0 +1,27 @@
+// preImmersive-buildingBlocksMastery-137-getLongestOfThreeWords*
+
+/*
+Write a function called “getLongestOfThreeWords”.
+
+Given 3 words, “getLongestOfThreeWords” returns the longest of three words.
+
+Notes:
+* If there is a tie, it should return the first word in the tie.
+
+var output = getLongestOfThreeWords('these', 'three', 'words');
+console.log(output); // --> 'these'
+*/
+
+function getLongestOfThreeWords(word1, word2, word3) {
+  return [...arguments].reduce( (longest, word) => (longest.length >= word.length) ? longest : word );
+}
+
+// function getLongestOfThreeWords(word1, word2, word3) {
+//   return [word1, word2, word3].reduce( (longest, word) => (longest.length >= word.length) ? longest : word );
+// }
+
+// function getLongestOfThreeWords(word1, word2, word3) {
+//   let longestWord =  (word1.length >= word2.length) ? word1 : word2;
+//   longestWord =  (longestWord.length >= word3.length) ? longestWord : word3;
+//   return longestWord;
+// }
