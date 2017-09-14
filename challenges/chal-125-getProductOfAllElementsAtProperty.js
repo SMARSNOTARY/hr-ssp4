7d1b1ac0d0a4b50a1b424748cb173a1dc6838351
Roger Sperberg <rsperberg@gmail.com>
2017-09-14 16:51:09 -0400

    Initial commit

---
+++ challenges/chal-125-getProductOfAllElementsAtProperty.js
@@ -0,0 +1,55 @@
+// preImmersive-buildingBlocksMastery-125-getProductOfAllElementsAtProperty*
+
+/*
+Write a function called “getProductOfAllElementsAtProperty”.
+
+Given an object and a key, “getProductOfAllElementsAtProperty” returns the product of all the elements in the array located at the given key.
+
+Notes:
+* If the array is empty, it should return 0.
+* If the property at the given key is not an array, it should return 0.
+* If there is no property at the given key, it should return 0.
+
+var obj = {
+  key: [1, 2, 3, 4]
+};
+var output = getProductOfAllElementsAtProperty(obj, 'key');
+console.log(output); // --> 24
+
+Your Code Should Pass:
+
+describe("getProductOfAllElementsAtProperty", function() {
+  it("should return the product of all the elements of the array located at key", function() {
+    var obj = {
+      array: [1, 2, 4]
+    };
+    expect(getProductOfAllElementsAtProperty(obj, "array")).toBe(8);
+  });
+  it("should return 0 if the array is empty", function() {
+    var obj = {
+      array: []
+    };
+    expect(getProductOfAllElementsAtProperty(obj, "array")).toBe(0);
+  });
+  it("should return 0 if the property is not an array", function() {
+    var obj = {
+      array: "sike"
+    };
+    expect(getProductOfAllElementsAtProperty(obj, "array")).toBe(0);
+  });
+  it("should return 0 if the property does not exist", function() {
+    var obj = {
+      what: "sike"
+    };
+    expect(getProductOfAllElementsAtProperty(obj, "array")).toBe(0);
+  });
+});
+*/
+
+function getProductOfAllElementsAtProperty(obj, key) {
+  var nums = obj[key];
+  if (!Array.isArray(nums) || nums === undefined || nums.length === 0) {
+    return 0;
+  }
+  return nums.reduce( (product, elem) => product * elem, 1 );
+}
