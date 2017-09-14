381fac17db575eeb310f2e1c97b8aef8a6ec8947
Roger Sperberg <rsperberg@gmail.com>
2017-09-14 17:02:31 -0400

    Add test descriptions

--- challenges/chal-150-convertObjectToList-2.js
+++ challenges/chal-150-convertObjectToList-2.js
@@ -25,6 +25,33 @@ Function’s return value (output):
 
 ['a', 11, true, 1]
 */
+/*
+Your Code Should Pass:
+
+describe ('listAllValues', function() {
+  it ('should_output_an_array_of_values', function(){
+    var input = {
+      foo: 'bar',
+      level: 1,
+      red: 'green',
+      number: true,
+      cry: 'excelsior'
+    }
+
+    var output = listAllValues(input);
+    console.log(Array.isArray(output));
+    console.log(output);
+
+    expect(output).not.toBeUndefined();
+    expect(output[0]).toBe('bar');
+    expect(output[1]).toBe(1);
+    expect(output[2]).toBe('green');
+    expect(output[3]).toBe(true);
+    expect(output[4]).toBe('excelsior');
+  });
+
+});
+*/
 
 function listAllValues(obj) {
   return Object.values(obj);
--- challenges/chal-151-convertObjectToList-3.js
+++ challenges/chal-151-convertObjectToList-3.js
@@ -14,6 +14,46 @@ Return value:
 
 [['name', 'Holly'], ['age', 35], ['role', 'producer']]
 */
+/*
+Your Code Should Pass:
+
+describe('convertObjectToList', function() {
+  it('converts_the_sample_data', function() {
+    var input = {
+      name: 'Holly',
+      age: 35,
+      role: 'producer'
+    };
+    var output = convertObjectToList(input);
+    expect(output[0][0]).toBe('name');
+    expect(output[0][1]).toBe('Holly');
+    expect(output[1][0]).toBe('age');
+    expect(output[1][1]).toBe(35);
+    expect(output[2][0]).toBe('role');
+    expect(output[2][1]).toBe('producer');
+  });
+});
+
+describe('convertObjectToList', function() {
+  it('converts_some_other_data', function() {
+    var input = {
+      foo: 'FOO',
+      bar: 'BAR',
+      baz: 123,
+      'another key': null
+    };
+    var output = convertObjectToList(input);
+    expect(output[0][0]).toBe('foo');
+    expect(output[0][1]).toBe('FOO');
+    expect(output[1][0]).toBe('bar');
+    expect(output[1][1]).toBe('BAR');
+    expect(output[2][0]).toBe('baz');
+    expect(output[2][1]).toBe(123);
+    expect(output[3][0]).toBe('another key');
+    expect(output[3][1]).toBe(null);
+  })
+});
+*/
 
 function convertObjectToList(obj) {
   let result = [];
