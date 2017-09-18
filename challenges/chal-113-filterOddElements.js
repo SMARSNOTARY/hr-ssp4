2634129a142def0c3d2f7b03c87fab223a4f824c
Roger Sperberg <rsperberg@gmail.com>
2017-09-18 16:07:21 -0400

    Initial commit

---
+++ challenges/chal-113-filterOddElements.js
@@ -0,0 +1,29 @@
+// preImmersive-buildingBlocksMastery-113-filterOddElements*
+
+/*
+Write a function called “filterOddElements”.
+
+Given an array of numbers, “filterOddElements” returns an array containing only the odd numbers of the given array.
+var output = filterOddElements([1, 2, 3, 4, 5]); console.log(output); // --> [1, 3, 5]
+
+Your Code Should Pass:
+
+describe("filterOddElements", function() {
+  it("should return an array", function() {
+    expect(Array.isArray(filterOddElements([1, 2, 3, 4]))).toBe(true);
+  });
+  it("should return an array with the odd elements from the passed in array", function() {
+    expect(filterOddElements([1, 2, 3, 4, 5])).toEqual([1, 3, 5]);
+  });
+  it("should return an array if there are no odd numbers", function() {
+    expect(filterOddElements([2, 4, 6])).toEqual([]);
+  });
+  it("should return an array if given an empty array", function() {
+    expect(filterOddElements([])).toEqual([]);
+  });
+});
+*/
+
+function filterOddElements(arr) {
+  return arr.filter( elem => elem % 2 === 1 );
+}
