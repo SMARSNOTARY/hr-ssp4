b65cb36ca0972d11c7c59f534eac618e4fe551b2
Roger Sperberg <rsperberg@gmail.com>
2017-09-24 15:29:56 -0400

    Initial commit

---
+++ challenges/chal-106-filterEvenLengthWords.js
@@ -0,0 +1,31 @@
+// preImmersive-buildingBlocksMastery-106-filterEvenLengthWords*
+
+/*
+Write a function called “filterEvenLengthWords”.
+
+Given an array of strings, “filterEvenLengthWords” returns an array containing only the elements of the given array whose length is an even number.
+
+var output = filterEvenLengthWords(['word', 'words', 'word', 'words']);
+console.log(output); // --> ['word', 'word']
+
+Your Code Should Pass:
+
+describe("filterEvenLengthWords", function() {
+  it("should return an array", function() {
+    expect(Array.isArray(filterEvenLengthWords(["there", "it", "is", "now"]))).toBe(true);
+  });
+  it("should return an array with even lengthed words", function() {
+    expect(filterEvenLengthWords(["there", "it", "is", "now"])).toEqual(["it", "is"]);
+  });
+  it("should return an empty array when passed an array with no even lengthed words", function() {
+    expect(filterEvenLengthWords(["there", "now"])).toEqual([]);
+  });
+  it("should return an empty array when passed an empty array", function() {
+    expect(filterEvenLengthWords([])).toEqual([]);
+  });
+});
+*/
+
+function filterEvenLengthWords(words) {
+  return words.filter( elem => elem.length % 2 === 0 );
+}
