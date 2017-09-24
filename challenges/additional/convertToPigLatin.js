7e97012bf1488c66d04d1b61683a925691d76f98
Roger Sperberg <rsperberg@gmail.com>
2017-09-24 15:26:14 -0400

    Initial commit

---
+++ challenges/additional/convertToPigLatin.js
@@ -0,0 +1,6 @@
+/*
+Write a function `convertToPigLatin` which takes a word or a string of words, and returns its pig latin translation.
+For simplified rules, see https://en.wikipedia.org/wiki/Pig_Latin#Rules
+
+[Try doing this in 30-40 mins, videoing yourself]
+*/
