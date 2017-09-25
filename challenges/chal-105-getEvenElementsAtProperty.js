7bbb6bca81398e9abc60a890342f317e353b1360
Roger Sperberg <rsperberg@gmail.com>
2017-09-24 20:28:43 -0400

    Initial commit

---
+++ challenges/chal-105-getEvenElementsAtProperty.js
@@ -0,0 +1,63 @@
+// preImmersive-buildingBlocksMastery-105-getEvenElementsAtProperty*
+
+/*
+Write a function called “getEvenElementsAtProperty”.
+
+Given an object and a key, “getEvenElementsAtProperty” returns an array containing all the even elements of the array located at the given key.
+
+Notes:
+* If the array is empty, it should return an empty array.
+* If the array contains no even elements, it should return an empty array.
+* If the property at the given key is not an array, it should return an empty array.
+* If there is no property at the given key, it should return an empty array.
+
+var obj = {
+  key: [1000, 11, 50, 17]
+};
+var output = getEvenElementsAtProperty(obj, 'key');
+console.log(output); // --> [1000, 50]
+
+Your Code Should Pass:
+
+describe("getEvenElementsAtProperty", function() {
+  it("should return an array containing all the even elements of the array located at key", function() {
+    var obj = {
+      array: [1, 2, 4]
+    };
+    expect(getEvenElementsAtProperty(obj, "array")).toEqual([2, 4]);
+  });
+  it("should return an empty array if the array has only no even elements", function() {
+    var obj = {
+      array: [1, 3]
+    };
+    expect(getEvenElementsAtProperty(obj, "array")).toEqual([]);
+  });
+  it("should return an empty array if the array is empty", function() {
+    var obj = {
+      array: []
+    };
+    expect(getEvenElementsAtProperty(obj, "array")).toEqual([]);
+  });
+  it("should return an empty array if the property is not an array", function() {
+    var obj = {
+      array: "sike"
+    };
+    expect(getEvenElementsAtProperty(obj, "array")).toEqual([]);
+  });
+  it("should return an empty array if the property does not exist", function() {
+    var obj = {
+      what: "sike"
+    };
+    expect(getEvenElementsAtProperty(obj, "array")).toEqual([]);
+  });
+});
+*/
+
+function getEvenElementsAtProperty(obj, key) {
+  var nums = obj[key];
+  if (!Array.isArray(nums) || nums === undefined || nums.length === 0) {
+    return [];
+  } else {
+    return nums.filter( elem => elem % 2 === 0 );
+  }
+}
