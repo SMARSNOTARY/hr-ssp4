af54325b5a49f3ec0e31986f89be88ee4f72baf9
Roger Sperberg <rsperberg@gmail.com>
2017-09-30 19:57:24 -0400

    Formatting change

--- challenges/chal-095-getNthElementOfProperty.js
+++ challenges/chal-095-getNthElementOfProperty.js
@@ -1,4 +1,4 @@
-preImmersive-buildingBlocksMastery-095-getNthElementOfProperty
+// preImmersive-buildingBlocksMastery-095-getNthElementOfProperty
 
 /*
 Write a function called “getNthElementOfProperty”.
