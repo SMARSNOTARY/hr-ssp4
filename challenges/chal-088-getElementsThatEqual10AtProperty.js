fea0e39d7fd93819a176139f9ad6a7313aad18a4
Roger Sperberg <rsperberg@gmail.com>
2017-10-07 13:14:31 -0400

    Initial commit

---
+++ challenges/chal-088-getElementsThatEqual10AtProperty.js
@@ -0,0 +1,104 @@
+// preImmersive-buildingBlocksMastery-088-getElementsThatEqual10AtProperty*
+
+/*
+Write a function called “getElementsThatEqual10AtProperty”.
+
+Given an object and a key, “getElementsThatEqual10AtProperty” returns an array containing all the elements of the array located at the given key that are equal to ten.
+
+Notes:
+* If the array is empty, it should return an empty array.
+* If the array contains no elements that are equal to 10, it should return an empty array.
+* If the property at the given key is not an array, it should return an empty array.
+* If there is no property at the key, it should return an empty array.
+
+var obj = {
+  key: [1000, 10, 50, 10]
+};
+var output = getElementsThatEqual10AtProperty(obj, 'key');
+console.log(output); // --> [10, 10]
+Your Code Should Pass:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+describe("getElementsThatEqual10AtProperty", function() {
+  it("should return an array containing all the elements that equal 10 in the array located at key", function() {
+    var obj = {
+      array: [10, 20, 40]
+    };
+    expect(getElementsThatEqual10AtProperty(obj, "array")).toEqual([10]);
+  });
+  it("should return an empty array if the array has no elements that equal 10", function() {
+    var obj = {
+      array: [1, 3]
+    };
+    expect(getElementsThatEqual10AtProperty(obj, "array")).toEqual([]);
+  });
+  it("should return an empty array if the array is empty", function() {
+    var obj = {
+      array: []
+    };
+    expect(getElementsThatEqual10AtProperty(obj, "array")).toEqual([]);
+  });
+  it("should return an empty array if the property is not an array", function() {
+    var obj = {
+      array: "sike"
+    };
+    expect(getElementsThatEqual10AtProperty(obj, "array")).toEqual([]);
+  });
+  it("should return an empty array if the property does not exist", function() {
+    var obj = {
+      what: "sike"
+    };
+    expect(getElementsThatEqual10AtProperty(obj, "array")).toEqual([]);
+  });
+});
+*/
+
+function getElementsThatEqual10AtProperty(obj, key) {
+  if ( !(Array.isArray(obj[key])) ) {
+    return [];
+  }
+  return obj[key].filter( elem => elem === 10 );
+}
+
+// function getElementsThatEqual10AtProperty(obj, key) {
+//   let resultArr = [];
+//   if ( !(Array.isArray(obj[key])) ) {
+//     return resultArr;
+//   }
+//   obj[key].forEach(function(element) {
+//     if (element === 10) {
+//       resultArr.push(10);
+//     }
+//   });
+//   return resultArr;
+// }
