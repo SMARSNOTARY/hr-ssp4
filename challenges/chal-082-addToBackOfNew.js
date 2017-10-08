a5dccb6fc73cbe2d85f5684d64fda9a02e18048a
Roger Sperberg <rsperberg@gmail.com>
2017-10-08 16:33:23 -0400

    Initial commit

---
+++ challenges/chal-082-addToBackOfNew.js
@@ -0,0 +1,39 @@
+// preImmersive-buildingBlocksMastery-082-addToBackOfNew
+
+/*
+Write a function called “addToBackNew”.
+
+Given an array and an element, “addToBackNew” returns a clone of the given array, with the given element added to the end.
+
+Important: It should be a NEW array instance, not the original array instance.
+
+var input = [1, 2];
+var output = addToBackOfNew(input, 3);
+console.log(input); // --> [1, 2]
+console.log(output); // --> [1, 2, 3]
+
+Your Code Should Pass:
+
+describe("addToBackOfNew", function() {
+  it("should return an array", function() {
+    expect(Array.isArray(addToBackOfNew([1, 2], 3))).toBe(true);
+  });
+  it("should add an element to the end of an array", function() {
+    expect(addToBackOfNew([1, 2], 3)).toEqual([1, 2, 3]);
+  });
+  it("should add an element to the end of an empty array", function() {
+    expect(addToBackOfNew([], 3)).toEqual([3]);
+  });
+  it("should leave arr unmodified", function() {
+    var originalArray = [1, 2];
+    var newArray = addToBackOfNew(originalArray, 3);
+    expect(originalArray).toEqual([1, 2]);
+  });
+});
+*/
+
+function addToBackOfNew(arr, element) {
+  var arr2 = arr.slice();
+  arr2.push(element);
+  return arr2;
+}
