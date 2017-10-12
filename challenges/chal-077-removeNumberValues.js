439002baf9985dfbb1152f61dfcb54a78436fc1d
Roger Sperberg <rsperberg@gmail.com>
2017-10-11 22:30:58 -0400

    Initial commit

---
+++ challenges/chal-077-removeNumberValues.js
@@ -0,0 +1,34 @@
+// preImmersive-buildingBlocksMastery-077-removeNumberValues
+
+/*
+Write a function called “removeNumberValues”.
+
+Given an object, “removeNumberValues” removes any properties whose values are numbers.
+
+var obj = {
+  a: 2,
+  b: 'remaining',
+  c: 4
+};
+removeNumberValues(obj);
+console.log(obj); // --> { b: 'remaining' }
+
+Your Code Should Pass:
+
+describe("removeNumberValues", function() {
+  it("should remove any properties with values that are numbers", function() {
+    var obj = {
+      a: [true, false],
+      b: 2,
+      c: [8, 0],
+      d: 4
+    };
+    var result = {
+      a: [true, false],
+      c: [8, 0]
+    };
+    removeNumberValues(obj);
+    expect(obj).toEqual(result);
+  });
+});
+*/
