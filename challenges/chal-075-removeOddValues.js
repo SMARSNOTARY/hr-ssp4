77f69209c45511dc0fb53390f487711c1c58f980
Roger Sperberg <rsperberg@gmail.com>
2017-10-12 21:04:02 -0400

    Initial commit

---
+++ challenges/chal-075-removeOddValues.js
@@ -0,0 +1,34 @@
+// preImmersive-buildingBlocksMastery-075-removeOddValues
+
+/*
+Write a function called “removeOddValues”.
+
+Given an object, “removeOddValues” removes any properties whose values are odd numbers.
+
+var obj = {
+  a: 2,
+  b: 3,
+  c: 4
+};
+removeOddValues(obj);
+console.log(obj); // --> { a: 2, c: 4 }
+
+Your Code Should Pass:
+
+describe("removeOddValues", function() {
+  it("should remove any properties with values that are odd numbers", function() {
+    var obj = {
+      a: 1,
+      b: 2,
+      c: 3,
+      d: 4
+    };
+    var result = {
+      b: 2,
+      d: 4
+    };
+    removeOddValues(obj);
+    expect(obj).toEqual(result);
+  });
+});
+*/
