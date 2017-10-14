e4874f4877cc0cdf13b5662f2fb9dbbcd0935ca7
Roger Sperberg <rsperberg@gmail.com>
2017-10-14 13:48:22 -0400

    Initial commit

---
+++ challenges/chal-072-removeStringValuesLongerThan.js
@@ -0,0 +1,35 @@
+// preImmersive-buildingBlocksMastery-072-removeStringValuesLongerThan
+
+/*
+Write a function called “removeStringValuesLongerThan”.
+
+Given an number and an object, “removeStringValuesLongerThan” removes any properties on the given object whose values are strings longer than the given number.
+
+var obj = {
+  name: 'Montana',
+  age: 20,
+  location: 'Texas'
+};
+removeStringValuesLongerThan(6, obj);
+console.log(obj); // { age: 20, location: 'Texas' }
+
+Your Code Should Pass:
+
+describe("removeStringValuesLongerThan", function() {
+  it("should remove any properties with values that are strings longer than num", function() {
+    var obj = {
+      a: "hello",
+      b: 2,
+      c: "montana",
+      d: 4
+    };
+    var result = {
+      a: "hello",
+      b: 2,
+      d: 4
+    };
+    removeStringValuesLongerThan(5, obj);
+    expect(obj).toEqual(result);
+  });
+});
+*/
