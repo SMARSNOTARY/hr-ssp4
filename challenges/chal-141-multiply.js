39cdc03808288946201fd46ed755cd8f3982fe9a
Roger Sperberg <rsperberg@gmail.com>
2017-10-21 15:41:07 -0400

    Deracinate

--- challenges/chal-141-multiply.js
+++ challenges/chal-141-multiply.js
@@ -11,18 +11,3 @@ Notes:
 var output = multiply(4, 7);
 console.log(output); // --> 28
 */
-
-function multiply(num1, num2) {
-  let total = 0;
-  let neg = (num1 < 0) ? (num2 < 0 ?false : true ) : (num2 < 0 ? true : false );
-  num1 = Math.abs(num1);
-  num2 = Math.abs(num2);
-  for (let i = 0; i < num2; i++) {
-    total += num1;
-  }
-  if (neg === true) {
-    return 0-total;
-  } else {
-    return total;
-  }
-}
