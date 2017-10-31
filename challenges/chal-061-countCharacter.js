733db99bae800ae628605511070f8428581dd332
Roger Sperberg <rsperberg@gmail.com>
2017-10-31 19:39:21 -0400

    Initial commit

---
+++ challenges/chal-061-countCharacter.js
@@ -0,0 +1,37 @@
+// preImmersive-buildingBlocksMastery-061-countCharacter
+
+/*
+Write a function called “countCharacter”.
+
+Given a string input and a character, “countCharacter” returns the number of occurences of a given character in the given string.
+
+var output = countCharacter('I am a hacker', 'a');
+console.log(output); // --> 3
+
+Your Code Should Pass:
+
+describe("countCharacter", function() {
+  it("should return a number", function() {
+    expect(typeof countCharacter("say what!?", "a")).toBe("number");
+  });
+  it("should return the number of occurences of a character in a string when the character exists", function() {
+    expect(countCharacter("say what!?", "a")).toBe(2);
+  });
+  it("should return the number of occurences of a character in a string when the character does not exist", function() {
+    expect(countCharacter("say what!?", "x")).toBe(0);
+  });
+  it("should return the number of occurences of a non-alphanumeric character in a string when the character exists", function() {
+    expect(countCharacter("say what!?", " ")).toBe(1);
+  });
+});
+*/
+
+function countCharacter(str, char) {
+  if (str.indexOf(char) === -1) {
+    return 0;
+  } else {
+    var regex = new RegExp(char, "gi");
+    var matches_array = str.match(regex);
+    return matches_array.length;
+  }
+}
