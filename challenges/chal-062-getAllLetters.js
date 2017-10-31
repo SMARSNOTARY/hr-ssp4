5877a9e38dffaf01a7fc5d2c543efc02536f9068
Roger Sperberg <rsperberg@gmail.com>
2017-10-30 21:36:43 -0400

    Initial commit

---
+++ challenges/chal-062-getAllLetters.js
@@ -0,0 +1,35 @@
+// preImmersive-buildingBlocksMastery-062-getAllLetters
+
+/*
+Write a function called “getAllLetters”.
+
+Given a word, “getAllLetters” returns an array containing every character in the word.
+
+Notes:
+* If given an empty string, it should return an empty array.
+
+var output = getAllLetters('Radagast');
+console.log(output); // --> ['R', 'a', 'd', 'a', 'g', 'a', 's', 't']
+
+Your Code Should Pass:
+
+describe("getAllLetters", function() {
+  it("should return an array", function() {
+    expect(Array.isArray(getAllLetters("something like this here"))).toBe(true);
+  });
+  it("should return an array containing all the letters in the word", function() {
+    expect(getAllLetters("Eowin")).toEqual(["E", "o", "w", "i", "n"]);
+  });
+  it("should return an empty array when given an empty string", function() {
+    expect(getAllLetters("")).toEqual([]);
+  });
+});
+*/
+
+function getAllLetters(str) {
+  let individualChars = [];
+  for (let i = 0; i < str.length; i++) {
+    individualChars.push(str[i])
+  }
+  return individualChars;
+}
