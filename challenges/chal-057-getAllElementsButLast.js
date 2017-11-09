e5a6114afa17206afb4bc7fbb4e89af79051a431
Roger Sperberg <rsperberg@gmail.com>
2017-11-08 21:30:28 -0500

    Initial commit

---
+++ challenges/chal-057-getAllElementsButLast.js
@@ -0,0 +1,32 @@
+// preImmersive-buildingBlocksMastery-057-getAllElementsButLast
+
+/*
+Write a function called “getAllElementsButLast”.
+
+Given an array, “getAllElementsButLast” returns an array with all the elements but the last.
+
+var input = [1, 2, 3, 4];
+var output = getAllElementsButLast(input);
+console.log(output); // --> [1, 2 , 3]
+
+Your Code Should Pass:
+
+describe("getAllElementsButLast", function() {
+  it("should return an array", function() {
+    expect(Array.isArray(getAllElementsButLast([4, 5, 6]))).toBe(true);
+  });
+  it("should return an array with all the elements of the passed in array, except for the last", function() {
+    expect(getAllElementsButLast([4, 5, 6])).toEqual([4, 5]);
+  });
+  it("should return an empty array when passed in a single element array", function() {
+    expect(getAllElementsButLast([4])).toEqual([]);
+  });
+  it("should return an empty array when passed in an empty array", function() {
+    expect(getAllElementsButLast([])).toEqual([]);
+  });
+});
+*/
+
+function getAllElementsButLast(array) {
+  return array.slice(0,-1);
+}
