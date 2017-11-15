24ca4e2782f8bf4980394a658f549c9758598519
Roger Sperberg <rsperberg@gmail.com>
2017-11-15 15:45:09 -0500

    Initial commit

---
+++ challenges/chal-051-doubleSquareRootOf.js
@@ -0,0 +1,24 @@
+// preImmersive-buildingBlocksMastery-051-doubleSquareRootOf
+
+/*
+Write a function called “doubleSquareRootOf”.
+Given a number, “doubleSquareRootOf” returns double its square root.
+
+var output = doubleSquareRootOf(121);
+console.log(output); // --> 22
+
+Your Code Should Pass:
+
+describe("doubleSquareRootOf", function() {
+  it("should return a number", function() {
+    expect(typeof doubleSquareRootOf(9)).toBe("number");
+  });
+  it("should return the doubled square root of the passed in number", function() {
+    expect(doubleSquareRootOf(9)).toBe(6);
+  });
+})
+*/
+
+function doubleSquareRootOf(num) {
+  return Math.sqrt(num) * 2;
+}
