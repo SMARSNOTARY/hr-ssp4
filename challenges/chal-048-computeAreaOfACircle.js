b1dc2bd8e35b16c4fc26c49a710847daf66fc387
Roger Sperberg <rsperberg@gmail.com>
2017-11-20 10:54:32 -0500

    Initial commit

---
+++ challenges/chal-048-computeAreaOfACircle.js
@@ -0,0 +1,27 @@
+// preImmersive-buildingBlocksMastery-048-computeAreaOfACircle
+
+/*
+Write a function called “computeAreaOfACircle”.
+
+Given the radius of a circle, “computeAreaOfACircle” returns its area.
+
+var output = computeAreaOfACircle(4);
+console.log(output); // --> 50.26548245743669
+Reference:
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/PI
+
+Your Code Should Pass:
+
+describe("computeAreaOfACircle", function() {
+  it("should return a number", function() {
+    expect(typeof computeAreaOfACircle(4)).toBe("number");
+  });
+  it("should return the area of a circle", function() {
+    expect(computeAreaOfACircle(4)).toBe(50.26548245743669);
+  });
+});
+*/
+
+function computeAreaOfACircle(radius) {
+  return Math.PI * radius * radius;
+}
