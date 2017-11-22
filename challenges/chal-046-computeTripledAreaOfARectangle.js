d2e54c4bd78097fe136b6658b5ebd6f69236a8a7
Roger Sperberg <rsperberg@gmail.com>
2017-11-22 13:01:52 -0500

    Initial commit

---
+++ challenges/chal-046-computeTripledAreaOfARectangle.js
@@ -0,0 +1,25 @@
+// preImmersive-buildingBlocksMastery-046-computeTripledAreaOfARectangle
+
+/*
+Write a function called “computeTripledAreaOfARectangle”.
+
+Given a length and width of a rectangle, “computeTripledAreaOfARectangle” returns the rectangle’s area, multiplied by 3.
+
+var output = computeTripledAreaOfARectangle(2, 4);
+console.log(output); // --> 24
+
+Your Code Should Pass:
+
+describe("computeTripledAreaOfARectangle", function() {
+  it("should return a number", function() {
+    expect(typeof computeTripledAreaOfARectangle(7, 6)).toBe("number");
+  });
+  it("should return the area of a rectangle", function() {
+    expect(computeTripledAreaOfARectangle(7, 6)).toBe(126);
+  });
+})
+*/
+
+function computeTripledAreaOfARectangle(length, width) {
+  return length * width * 3;
+}
