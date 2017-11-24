2821993c88e9196a644d5a78629b360b1656ee78
Roger Sperberg <rsperberg@gmail.com>
2017-11-24 13:45:41 -0500

    Initial commit

---
+++ challenges/chal-044-computePerimeterOfARectangle.js
@@ -0,0 +1,25 @@
+// challenge-044-computePerimeterOfARectangle
+
+/*
+Write a function called “computePerimeterOfARectangle”.
+
+Given a length and a width describing a rectangle, “computePerimeterOfARectangle” returns its perimter.
+
+var output = computePerimeterOfARectangle(5, 2);
+console.log(output); // --> 14
+
+Your Code Should Pass:
+
+describe("computePerimeterOfARectangle", function() {
+  it("should return a number", function() {
+    expect(typeof computePerimeterOfARectangle(5, 2)).toBe("number");
+  });
+  it("should return the perimeter of a rectangle", function() {
+    expect(computePerimeterOfARectangle(5, 2)).toBe(14);
+  });
+})
+*/
+
+function computePerimeterOfARectangle(length, width) {
+  return (length + width) * 2;
+}
