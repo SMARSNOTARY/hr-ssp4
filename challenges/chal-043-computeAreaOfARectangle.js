c1321c60f9e26db309601bdf11107d7a1bd46e12
Roger Sperberg <rsperberg@gmail.com>
2017-11-25 19:42:01 -0500

    Initial commit

---
+++ challenges/chal-043-computeAreaOfARectangle.js
@@ -0,0 +1,25 @@
+// challenge-043-computeAreaOfARectangle
+
+/*
+Write a function called “computeAreaOfARectangle”.
+
+Given the length and width of a rectangle, “computeAreaOfARectangle” returns its area.
+
+var output = computeAreaOfARectangle(4, 8);
+console.log(output); // --> 32
+
+Your Code Should Pass:
+
+describe("computeAreaOfARectangle", function() {
+  it("should return a number", function() {
+    expect(typeof computeAreaOfARectangle(7, 6)).toBe("number");
+  });
+  it("should return the area of a rectangle", function() {
+    expect(computeAreaOfARectangle(7, 6)).toBe(42);
+  });
+})
+*/
+
+function computeAreaOfARectangle(length, width) {
+  return length * width;
+}
