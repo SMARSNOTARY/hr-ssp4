370acda66eafbee22e35f4886c25c6401a220cd2
Roger Sperberg <rsperberg@gmail.com>
2017-12-02 16:09:55 -0500

    Initial commit

---
+++ challenges/chal-036-isPersonOldEnoughToDrink.js
@@ -0,0 +1,52 @@
+// challenge-036-isPersonOldEnoughToDrink
+
+/*
+Write a function called “isPersonOldEnoughToDrink”.
+
+Given a “person” object, that contains an “age” property, “isPersonOldEnoughToDrink” returns whether the given person is old enough to drink.
+
+Notes:
+* The legal drinking age in the United States is 21.
+
+var obj = {
+  age: 16
+};
+var output = isPersonOldEnoughToDrink(obj);
+console.log(output); // --> false
+
+Your Code Should Pass:
+
+describe("isPersonOldEnoughToDrink", function() {
+  it("should return a boolean", function() {
+    var person = {
+      age: 55
+    };
+    expect(typeof isPersonOldEnoughToDrink(person)).toBe("boolean");
+  });
+  it("should return true if a person has an age of over 21", function() {
+    var person = {
+      age: 55
+    };
+    expect(isPersonOldEnoughToDrink(person)).toBe(true);
+  });
+  it("should return true if a person has an age of 21", function() {
+    var person = {
+      age: 21
+    };
+    expect(isPersonOldEnoughToDrink(person)).toBe(true);
+  });
+  it("should return false if a person has an age under 21", function() {
+    var person = {
+      age: 20
+    };
+    expect(isPersonOldEnoughToDrink(person)).toBe(false);
+  });
+});
+*/
+
+function isPersonOldEnoughToDrink(person) {
+    if (person === undefined) {
+    return false;
+  }
+  return person.age >= 21;
+}
