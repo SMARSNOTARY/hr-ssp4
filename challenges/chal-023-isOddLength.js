8d151225803d0b989073fba2cd5c11eff476d822
Roger Sperberg <rsperberg@gmail.com>
2017-12-15 15:40:05 -0500

    Initial commit

---
+++ challenges/chal-023-isOddLength.js
@@ -0,0 +1,28 @@
+// challenge-023-isOddLength
+
+/*
+Write a function called “isOddLength”.
+
+Given a word, “isOddLength” returns whether the length of the given word is odd.
+
+var output = isOddLength('special');
+console.log(output); // --> true
+
+Your Code Should Pass:
+
+describe("isOddLength", function() {
+  it("should return a boolean", function() {
+    expect(typeof isOddLength("wow")).toEqual("boolean");
+  });
+  it("should return if the length of the word is even", function() {
+    expect(isOddLength("special")).toBe(true);
+  });
+  it("should return false if the string is empty", function() {
+    expect(isOddLength("")).toBe(false);
+  });
+});
+*/
+
+function isOddLength(word) {
+  return word.length % 2 !== 0;
+}
