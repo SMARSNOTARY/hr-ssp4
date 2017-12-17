022473fb534c62e368a649396a8e93aea6322ae0
Roger Sperberg <rsperberg@gmail.com>
2017-12-17 14:12:02 -0500

    Initial commit

---
+++ challenges/chal-021-areBothOdd.js
@@ -0,0 +1,27 @@
+// challenge-021-areBothOdd
+
+/*
+Write a function called “areBothOdd”.
+
+Given 2 numbers, “areBothOdd” returns whether or not both of the given numbers are odd.
+
+var output = areBothOdd(1, 3);
+console.log(output); // --> true
+
+Your Code Should Pass:
+
+describe("areBothOdd", function() {
+  it("should return a boolean", function() {
+    expect(typeof areBothOdd(40, 3)).toEqual("boolean");
+  });
+  it("should return true if both numbers are odd", function() {
+    expect(areBothOdd(41, 3)).toBe(true);
+  });
+  it("should return false if the first number is even", function() {
+    expect(areBothOdd(4, 3)).toBe(false);
+  });
+  it("should return false if the second number is even", function() {
+    expect(areBothOdd(4, 30)).toBe(false);
+  });
+});
+*/
