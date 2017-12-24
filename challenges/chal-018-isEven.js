e1c3cfd8d10ed643ee616c145454908100723b6b
Roger Sperberg <rsperberg@gmail.com>
2017-12-24 13:28:25 -0500

    Initial commit

---
+++ challenges/chal-018-isEven.js
@@ -0,0 +1,27 @@
+// challenge-018-isEven
+
+/*
+Write a function called “isEven”.
+Given a number, “isEven” returns whether it is even.
+
+var output = isEven(11);
+console.log(output); // --> false
+
+Your Code Should Pass:
+
+describe("isEven", function() {
+  it("should return a boolean", function() {
+    expect(typeof isEven(40)).toEqual("boolean");
+  });
+  it("should return if the number is even", function() {
+    expect(isEven(8)).toBe(true);
+  });
+  it("should return true if the number is 0", function() {
+    expect(isEven(0)).toBe(true);
+  });
+});
+*/
+
+function isEven(num) {
+  return num % 2 === 0;
+}
