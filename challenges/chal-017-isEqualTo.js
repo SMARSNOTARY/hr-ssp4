9d0d705c6d5a1b32d76b09cdef430109d80f50ed
Roger Sperberg <rsperberg@gmail.com>
2017-12-25 12:54:23 -0500

    Initial commit

---
+++ challenges/chal-017-isEqualTo.js
@@ -0,0 +1,30 @@
+// challenge-017-isEqualTo
+
+/*
+Write a function called “isEqualTo”.
+Given 2 numbers, “isEqualTo” returns whether num2 is equal to num1.
+
+var output = isEqualTo(11, 10);
+console.log(output); // --> false
+
+Your Code Should Pass:
+
+describe("isEqualTo", function() {
+  it("should return a boolean", function() {
+    expect(typeof isEqualTo(40, 30)).toEqual("boolean");
+  });
+  it("should return false if num2 is greater than num1", function() {
+    expect(isEqualTo(20, 200)).toBe(false);
+  });
+  it("should return false if num2 is less than num1", function() {
+    expect(isEqualTo(20, 2)).toBe(false);
+  });
+  it("should return true if the numbers are equal", function() {
+    expect(isEqualTo(20, 20)).toBe(true);
+  });
+});
+*/
+
+function isEqualTo(num1, num2) {
+  return num2 === num1;
+}
