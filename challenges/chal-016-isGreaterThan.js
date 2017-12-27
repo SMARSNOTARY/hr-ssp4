4341c8fc0f890cad7c2aa8d6cccc5004ee6de0a6
Roger Sperberg <rsperberg@gmail.com>
2017-12-26 21:04:55 -0500

    Initial commit

---
+++ challenges/chal-016-isGreaterThan.js
@@ -0,0 +1,27 @@
+// challenge-016-isGreaterThan
+
+/*
+Write a function called “isGreaterThan”.
+Given 2 numbers, “isGreaterThan” returns whether num2 is greater than num1.
+
+var output = isGreaterThan(11, 10);
+console.log(output); // --> false
+
+Your Code Should Pass:
+
+describe("isGreaterThan", function() {
+  it("should return a boolean", function() {
+    expect(typeof isGreaterThan(40, 30)).toEqual("boolean");
+  });
+  it("should return whether num2 is greater than num1", function() {
+    expect(isGreaterThan(20, 200)).toBe(true);
+  });
+  it("should return false if the numbers are equal", function() {
+    expect(isGreaterThan(20, 20)).toBe(false);
+  });
+});
+*/
+
+function isGreaterThan(num1, num2) {
+  return num2 > num1;
+}
