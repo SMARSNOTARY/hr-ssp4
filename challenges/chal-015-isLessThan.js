bcfa8c55ed2075bf58b214e048c11f5b07fcf118
Roger Sperberg <rsperberg@gmail.com>
2017-12-28 14:32:43 -0500

    Initial commit

---
+++ challenges/chal-015-isLessThan.js
@@ -0,0 +1,27 @@
+// challenge-015-isLessThan
+
+/*
+Write a function called “isLessThan”.
+Given 2 numbers, “isLessThan” returns whether num2 is less than num1.
+
+var output = isLessThan(9, 4);
+console.log(output); // --> true
+
+Your Code Should Pass:
+
+describe("isLessThan", function() {
+  it("should return a boolean", function() {
+    expect(typeof isLessThan(40, 30)).toEqual("boolean");
+  });
+  it("should return whether num2 is less than num1", function() {
+    expect(isLessThan(20, 200)).toBe(false);
+  });
+  it("should return false if the numbers are equal", function() {
+    expect(isLessThan(20, 20)).toBe(false);
+  });
+});
+*/
+
+function isLessThan(num1, num2) {
+  return num2 < num1;
+}
