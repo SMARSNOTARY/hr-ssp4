acafd882327bc1f33287e4ea4c3af9b050e3c1d1
Roger Sperberg <rsperberg@gmail.com>
2017-12-30 13:52:13 -0500

    Initial commit

---
+++ challenges/chal-014-equalsTen.js
@@ -0,0 +1,30 @@
+// challenge-014-equalsTen
+
+/*
+Write a function called “equalsTen”.
+Given a number, “equalsTen” returns whether or not the given number is 10.
+
+var output = equalsTen(9);
+console.log(output); // --> false
+
+Your Code Should Pass:
+
+describe("equalsTen", function() {
+  it("should return a boolean", function() {
+    expect(typeof equalsTen(10)).toEqual("boolean");
+  });
+  it("should return true for the number 10", function() {
+    expect(equalsTen(10)).toBe(true);
+  });
+  it("should return false for a number greater than 10", function() {
+    expect(equalsTen(400)).toBe(false);
+  });
+  it("should return false for the number 10", function() {
+    expect(equalsTen(3)).toBe(false);
+  });
+});
+*/
+
+function equalsTen(num) {
+  return num === 10;
+}
