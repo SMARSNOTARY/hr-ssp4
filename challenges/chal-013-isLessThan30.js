8222b27bad1665db799346ef045f936e94e3a8c6
Roger Sperberg <rsperberg@gmail.com>
2017-12-31 17:21:00 -0500

    Initial commit

---
+++ challenges/chal-013-isLessThan30.js
@@ -0,0 +1,30 @@
+// challenge-013-isLessThan30
+
+/*
+Write a function called “isLessThan30”.
+Given a number, “isLessThan30” returns whether the given number is less than 30.
+
+var output = isLessThan30(9);
+console.log(output); // --> true
+
+Your Code Should Pass:
+
+describe("isLessThan30", function() {
+  it("should return a boolean", function() {
+    expect(typeof isLessThan30(40)).toEqual("boolean");
+  });
+  it("should return true for a number less than 30", function() {
+    expect(isLessThan30(4)).toBe(true);
+  });
+  it("should return false for a number greater than 30", function() {
+    expect(isLessThan30(400)).toBe(false);
+  });
+  it("should return false for the number 30", function() {
+    expect(isLessThan30(30)).toBe(false);
+  });
+});
+*/
+
+function isLessThan30(num) {
+  return num < 30;
+}
