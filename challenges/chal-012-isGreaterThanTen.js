3f448dfe96b33d088d4b8274715003b151c63a9e
Roger Sperberg <rsperberg@gmail.com>
2018-01-01 16:27:49 -0500

    Initial commit

---
+++ challenges/chal-012-isGreaterThanTen.js
@@ -0,0 +1,30 @@
+// challenge-012-isGreaterThanTen
+
+/*
+Write a function called “isGreaterThan10”.
+Given a number, “isGreaterThan10” returns whether the given number is greater than 10.
+
+var output = isGreaterThan10(11);
+console.log(output); // --> true
+
+Your Code Should Pass:
+
+describe("isGreaterThan10", function() {
+  it("should return a boolean", function() {
+    expect(typeof isGreaterThan10(40)).toEqual("boolean");
+  });
+  it("should return false for a number less than 10", function() {
+    expect(isGreaterThan10(4)).toBe(false);
+  });
+  it("should return true for a number greater than 10", function() {
+    expect(isGreaterThan10(40)).toBe(true);
+  });
+  it("should return false for the number 10", function() {
+    expect(isGreaterThan10(10)).toBe(false);
+  });
+});
+*/
+
+function isGreaterThan10(num) {
+  return num > 10;
+}
