13499af89301c46cf663dcc8f0207a427369a984
Roger Sperberg <rsperberg@gmail.com>
2018-01-02 14:57:47 -0500

    Initial commit

---
+++ challenges/chal-011-getLengthOfTwoWords.js
@@ -0,0 +1,24 @@
+// challenge-011-getLengthOfTwoWords
+
+/*
+Write a function called “getLengthOfTwoWords”.
+Given 2 words, “getLengthOfTwoWords” returns the sum of their lengths.
+
+var output = getLengthOfTwoWords('some', 'words');
+console.log(output); // --> 9
+
+Your Code Should Pass:
+
+describe("getLengthOfTwoWords", function() {
+  it("should return a number", function() {
+    expect(typeof getLengthOfTwoWords("two", "words")).toBe("number");
+  });
+  it("should return the sum length of two words", function() {
+    expect(getLengthOfTwoWords("one", "two")).toBe(6)
+  });
+});
+*/
+
+function getLengthOfTwoWords(word1, word2) {
+  return word1.length + word2.length;
+}
