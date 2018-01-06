56db4a98bdb53b122ed5ab778f04b6b5b62cd853
Roger Sperberg <rsperberg@gmail.com>
2018-01-06 14:20:54 -0500

    Initial commit

---
+++ challenges/chal-010-getLengthOfWord.js
@@ -0,0 +1,26 @@
+// challenge-010-getLengthOfWord
+
+/*
+Write a function called “getLengthOfWord”.
+Given a word, “getLengthOfWord” returns the length of the given word.
+
+var output = getLengthOfWord('some');
+console.log(output); // --> 4
+Your Code Should Pass:
+
+describe("getLengthOfWord", function() {
+  it("should return a number", function() {
+    expect(typeof getLengthOfWord("something")).toBe("number");
+  });
+  it("should return the length of the passed in word", function() {
+    expect(getLengthOfWord("random")).toBe(6)
+  });
+  it("should return the length of an empty word", function() {
+    expect(getLengthOfWord("")).toBe(0)
+  });
+});
+*/
+
+function getLengthOfWord(word) {
+  return word.length;
+}
