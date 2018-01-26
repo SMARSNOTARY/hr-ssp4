87d6574c3ddf9a53923be198d0ff3ef877f83590
Roger Sperberg <rsperberg@gmail.com>
2018-01-26 18:10:24 -0500

    Initial commit

---
+++ challenges/chal-003-isOldEnoughToVote.js
@@ -0,0 +1,29 @@
+// challenge-003-isOldEnoughToVote
+
+/*
+Write a function called “isOldEnoughToVote”.
+Given a number, in this case an age, ‘isOldEnoughToVote’ returns whether a person of this given age is old enough to legally vote in the United States.
+Notes:
+* The legal voting age in the United States is 18.
+
+var output = isOldEnoughToVote(22);
+console.log(output); // --> true
+
+Your Code Should Pass:
+
+describe("isOldEnoughToVote", function() {
+  it("should return a boolean", function() {
+    expect(typeof isOldEnoughToVote(40)).toEqual("boolean");
+  });
+  it("should return whether the age is greater than 18", function() {
+    expect(isOldEnoughToVote(40)).toBe(true);
+  });
+  it("should return true if the age is 18", function() {
+    expect(isOldEnoughToVote(18)).toBe(true);
+  });
+});
+*/
+
+function isOldEnoughToVote(age) {
+  return ( age >= 18 );
+}
