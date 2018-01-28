535e7a8c9ac16e53350dafcd2a0683c6db376104
Roger Sperberg <rsperberg@gmail.com>
2018-01-27 20:11:32 -0500

    Initial commit

---
+++ challenges/chal-002-isOldEnoughToDrive.js
@@ -0,0 +1,29 @@
+// challenge-002-isOldEnoughToDrive
+
+/*
+Write a function called “isOldEnoughToDrive”.
+Given a number, in this case an age, “isOldEnoughToDrive” returns whether a person of this given age is old enough to legally drive in the United States.
+Notes:
+* The legal driving age in the United States is 16.
+
+var output = isOldEnoughToDrive(22);
+console.log(output); // --> true
+
+Your Code Should Pass:
+
+describe("isOldEnoughToDrive", function() {
+  it("should return a boolean", function() {
+    expect(typeof isOldEnoughToDrive(40)).toEqual("boolean");
+  });
+  it("should return true if the age is 16", function() {
+    expect(isOldEnoughToDrive(16)).toBe(true);
+  });
+  it("should return false if the age is less than 16", function() {
+    expect(isOldEnoughToDrive(15)).toBe(false);
+  });
+});
+*/
+
+function isOldEnoughToDrive(age) {
+  return( age >= 16 );
+}
